9732e839dfe4032545dcde329f8c24bdc0878351
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2021-04-01 10:37:28 +0200

    init

---
+++ addons/[ttt]_aug_(css)_253736374/gamemodes/terrortown/entities/weapons/weapon_ttt_aug/shared.lua
@@ -0,0 +1,73 @@
+--[[Author informations]]--
+SWEP.Author = "Zaratusa"
+SWEP.Contact = "http://steamcommunity.com/profiles/76561198032479768"
+
+if SERVER then
+	AddCSLuaFile()
+	resource.AddWorkshop("253736374")
+else
+	LANG.AddToLanguage("english", "aug_name", "AUG")
+
+	SWEP.PrintName = "aug_name"
+	SWEP.Slot = 2
+	SWEP.Icon = "vgui/ttt/icon_aug"
+
+	-- client side model settings
+	SWEP.UseHands = true -- should the hands be displayed
+	SWEP.ViewModelFlip = false -- should the weapon be hold with the left or the right hand
+	SWEP.ViewModelFOV = 60
+end
+
+-- always derive from weapon_tttbase
+SWEP.Base = "weapon_tttbase"
+
+--[[Default GMod values]]--
+SWEP.Primary.Ammo = "SMG1"
+SWEP.Primary.Delay = 0.09
+SWEP.Primary.Recoil = 1.04
+SWEP.Primary.Cone = 0.025
+SWEP.Primary.Damage = 17
+SWEP.Primary.Automatic = true
+SWEP.Primary.ClipSize = 30
+SWEP.Primary.ClipMax = 60
+SWEP.Primary.DefaultClip = 30
+SWEP.Primary.Sound = Sound("Weapon_AUG.Single")
+
+
+--[[Model settings]]--
+SWEP.HoldType = "ar2"
+SWEP.ViewModel = Model("models/weapons/cstrike/c_rif_aug.mdl")
+SWEP.WorldModel = Model("models/weapons/w_rif_aug.mdl")
+
+SWEP.IronSightsPos = Vector(5, -15, -2)
+SWEP.IronSightsAng = Vector(2.6, 1.37, 3.5)
+
+--[[TTT config values]]--
+
+-- Kind specifies the category this weapon is in. Players can only carry one of
+-- each. Can be: WEAPON_... MELEE, PISTOL, HEAVY, NADE, CARRY, EQUIP1, EQUIP2 or ROLE.
+-- Matching SWEP.Slot values: 0      1       2     3      4      6       7        8
+SWEP.Kind = WEAPON_HEAVY
+
+-- If AutoSpawnable is true and SWEP.Kind is not WEAPON_EQUIP1/2,
+-- then this gun can be spawned as a random weapon.
+SWEP.AutoSpawnable = true
+
+-- The AmmoEnt is the ammo entity that can be picked up when carrying this gun.
+SWEP.AmmoEnt = "item_ammo_smg1_ttt"
+
+-- If AllowDrop is false, players can't manually drop the gun with Q
+SWEP.AllowDrop = true
+
+-- If IsSilent is true, victims will not scream upon death.
+SWEP.IsSilent = false
+
+function SWEP:SecondaryAttack()
+
+end
+
+function SWEP:Reload()
+	if (self:Clip1() < self.Primary.ClipSize and self.Owner:GetAmmoCount(self.Primary.Ammo) > 0) then
+		self:DefaultReload(ACT_VM_RELOAD)
+	end
+end
